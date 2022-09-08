5c9a2036dd0b5a16e58db10274443348e2b0ae2e
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-09-08 12:15:07 -0500

    add reset canvas (beginPath)

--- js/control.js
+++ js/control.js
@@ -1,17 +1,19 @@
 const juegonuevo = document.getElementById('juegonuevo');
+const juegonuevo2 = document.getElementById('juegonuevo2');
 const agregarpalabra = document.getElementById('agregarpalabra');
 const div_canvas = document.getElementById('div_canvas');
 const div_palabra = document.getElementById('div_palabra');
 const input_teclado = document.getElementById('input_teclado');
+const desistir = document.getElementById('desistir');
+const ap_cancelar = document.getElementById('ap_cancelar');
 
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 var WIDTH = screen.width;
-var HEIGHT = 500;
+var HEIGHT = 420;
 var COLOR = "#309694";
-const body = document.getElementById('body');
 var gamemode = false;
-var errores = 0;
+var ERRORES = 0;
 
 //start
 scrrsz();
@@ -32,21 +34,48 @@ function scrrsz() {
     dibujaAhorcado(WIDTH / 2 - 50, HEIGHT / 2);
 }
 
-juegonuevo.addEventListener('click', function handleClick() {
+function juegoNuevo() {
+    ERRORES = 0;
+    div_botones.style.display = "none";
     div_canvas.style.display = "block";
     div_palabra.style.display = "none";
     gamemode = true;
+    limpiarPantalla();
     dibujaAhorcado(WIDTH / 2 - 50, HEIGHT / 2);
+}
+
+juegonuevo.addEventListener('click', function handleClick() {
+    juegoNuevo();
 });
 
+juegonuevo2.addEventListener('click', function handleClick() {
+    juegoNuevo();
+});
+
+function pantallaInicial() {
+    div_botones.style.display = "block";
+    div_canvas.style.display = "none";
+    div_palabra.style.display = "none";
+}
+
+desistir.addEventListener('click', function handleClick() {
+    pantallaInicial();
+});
+
+ap_cancelar.addEventListener('click', function handleClick() {
+    pantallaInicial();
+});
+
+
 agregarpalabra.addEventListener('click', function handleClick() {
+    div_botones.style.display = "none";
     div_canvas.style.display = "none";
     div_palabra.style.display = "block";
     gamemode = false;
 });
 
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keyup', e => {
     if (e.repeat) {
         e.preventDefault();
         return;
@@ -64,9 +93,10 @@ document.addEventListener('keydown', e => {
                 console.log("Presionaste una vocal");
                 break;
             default:
-                errores += 1;
-                console.log("Presionaste otra letra que no es vocal | errores: " + errores);
+                ERRORES += 1;
+                console.log("Presionaste otra letra que no es vocal | ERRORES: " + ERRORES);
         }
+        input_teclado.value = "";
         dibujaAhorcado(WIDTH / 2 - 50, HEIGHT / 2);
     }
 });
@@ -78,8 +108,9 @@ function dibujaAhorcado(x, y) {
 
     pincel.fillStyle = "black";
     pincel.lineWidth = 10;
+    pincel.beginPath();
 
-    if (errores > 0) {
+    if (ERRORES > 0) {
         //0 base
         pincel.moveTo(x - 50, y);
         pincel.lineTo(x + 150, y);
@@ -87,15 +118,15 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 1) {
+    if (ERRORES > 1) {
         //1 poste
         pincel.moveTo(x, y);
         pincel.lineTo(x, y - 200);
         pincel.stroke();
-
+        console.log("Entro aqui cuando hay 2 errores");
     }
 
-    if (errores > 2) {
+    if (ERRORES > 2) {
         //2 barra
         pincel.moveTo(x - 5, y - 200);
         pincel.lineTo(x + 100, y - 200);
@@ -103,7 +134,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 3) {
+    if (ERRORES > 3) {
         //3 cuerda
         pincel.moveTo(x + 100, y - 200 - 5);
         pincel.lineTo(x + 100, y - 160);
@@ -111,7 +142,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 4) {
+    if (ERRORES > 4) {
         //4 cabeza
         pincel.beginPath();
         pincel.arc(x + 100, y - 160 + 20, 20, 0, 2 * Math.PI);
@@ -119,7 +150,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 5) {
+    if (ERRORES > 5) {
         //5 tronco
         pincel.moveTo(x + 100, y - 120);
         pincel.lineTo(x + 100, y - 50);
@@ -127,7 +158,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 6) {
+    if (ERRORES > 6) {
         //6 brazo izq
         pincel.moveTo(x + 100, y - 120);
         pincel.lineTo(x + 70, y - 120 + 30);
@@ -135,7 +166,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 7) {
+    if (ERRORES > 7) {
         //7 brazo der
         pincel.moveTo(x + 100, y - 120);
         pincel.lineTo(x + 130, y - 120 + 30);
@@ -143,7 +174,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 8) {
+    if (ERRORES > 8) {
         //8 pie izq
         pincel.moveTo(x + 100, y - 50 - 5);
         pincel.lineTo(x + 70, y - 50 + 30);
@@ -151,7 +182,7 @@ function dibujaAhorcado(x, y) {
 
     }
 
-    if (errores > 9) {
+    if (ERRORES > 9) {
         //9 pie der
         pincel.moveTo(x + 100, y - 50 - 5);
         pincel.lineTo(x + 130, y - 50 + 30);
@@ -168,4 +199,5 @@ function limpiarPantalla() {
     pincel.clearRect(0, 0, WIDTH, HEIGHT);
     pincel.fillStyle = COLOR;
     pincel.fillRect(0, 0, WIDTH, HEIGHT);
+
 }
